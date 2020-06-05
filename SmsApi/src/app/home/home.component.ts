7f916bd2ad6d6e10af38caf16e7e85a7cdc188f1
Michael <michel.wojciech@gmail.com>
2020-06-05 23:39:33 +0200

    correct apostrophe

--- SmsApi/src/app/home/home.component.ts
+++ SmsApi/src/app/home/home.component.ts
@@ -11,21 +11,17 @@ export class HomeComponent implements OnInit {
   title: string;
 
   constructor() {
-    this.title = "Wygląda na to, że wszystko działa jak należy!"
+    this.title = 'Wygląda na to, że wszystko działa jak należy!';
   }
 
-  // Flaga static jest wymaga od wersji Angular 8
-  // Zmiana ta związana jest cyklem życia strony oraz dostępnością danych
-  // Więcej tutaj: https://medium.com/angular-in-depth/embrace-yourself-angular-8-is-coming-1bf187c8f0bf
   @ViewChild('f', { static: false }) carForm: NgForm;
 
   onSubmit(form: NgForm) {
-    // Przekaliśmy naszą zmienną lokalną 'f' z formularza dzięki czemu mamy dostęp do poszczególnych pól
-    console.log("Marka samochodu: " + form.value.carBrand);
-    console.log("Opis samochodu: " + form.value.carDescription);
-    console.log("Wielkość zamówienia: " + form.value.carAmount);
+    console.log('Marka samochodu: ' + form.value.carBrand);
+    console.log('Opis samochodu: ' + form.value.carDescription);
+    console.log('Wielkość zamówienia: ' + form.value.carAmount);
   }
-  
+
   onClear() {
     this.carForm.reset();
   }
