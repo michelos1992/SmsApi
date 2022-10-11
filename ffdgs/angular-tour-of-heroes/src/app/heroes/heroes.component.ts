10d077c68af1dbbf62549d37bfdf22674d720088
Michal <michel.wojciech@gmail.com>
2022-10-11 18:02:32 +0200

    create message service and component

--- ffdgs/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ ffdgs/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,16 +10,10 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  //heroes = HEROES;
   selectedHero?: Hero;
   heroes: Hero[] = [];
 
-  // hero: Hero = {
-  //   id: 1,
-  //   name: 'Windstorm'
-  // };
-
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
 
   ngOnInit(): void {
     this.getHeroes();
@@ -26,11 +21,9 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
-  // getHeroes(): void {
-  //   this.heroes = this.heroService.getHeroes();
-  // }
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
