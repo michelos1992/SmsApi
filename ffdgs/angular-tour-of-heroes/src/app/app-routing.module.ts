75de4e0a41fe437666f81ea422df0de05d7f54e3
Michael <michel.wojciech@gmail.com>
2023-01-24 23:15:17 +0100

    add routing and path

--- ffdgs/angular-tour-of-heroes/src/app/app-routing.module.ts
+++ ffdgs/angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+import { HeroesComponent } from './heroes/heroes.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
