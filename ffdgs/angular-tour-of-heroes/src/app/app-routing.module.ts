679217e7b7dc43aa3073b5aff620e1240fd288b9
Michael <michel.wojciech@gmail.com>
2023-01-27 00:02:06 +0100

    get one hero to dash and add detail hero

--- ffdgs/angular-tour-of-heroes/src/app/app-routing.module.ts
+++ ffdgs/angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
-import { HeroesComponent } from './heroes/heroes.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'heroes', component: HeroesComponent},
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
 ];
 
 @NgModule({
--- ffdgs/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ ffdgs/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -10,7 +10,7 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero?: Hero;
+  //selectedHero?: Hero;
   heroes: Hero[] = [];
 
   constructor(private heroService: HeroService, private messageService: MessageService) {}
@@ -20,7 +20,7 @@ export class HeroesComponent implements OnInit {
   }
 
   onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+    ///this.selectedHero = hero;
     this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
