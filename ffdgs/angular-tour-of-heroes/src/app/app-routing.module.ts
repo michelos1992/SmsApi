3bdc0216db00ba8fba0c9a96caedb40b623cd482
Michal <michel.wojciech@gmail.com>
2023-06-28 17:31:34 +0200

    changes accepted

--- ffdgs/angular-tour-of-heroes/src/app/app-routing.module.ts
+++ ffdgs/angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -2,11 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeroesComponent } from './heroes/heroes.component';
-<<<<<<< HEAD
-
-const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent }
-=======
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
@@ -15,18 +10,13 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'heroes', component: HeroesComponent},
->>>>>>> 68d351b645b450a97a2af4e7239bdf3467bd2985
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes)
   ],
-<<<<<<< HEAD
-  exports: [RouterModule],
-=======
   exports: [RouterModule]
->>>>>>> 68d351b645b450a97a2af4e7239bdf3467bd2985
 })
 export class AppRoutingModule { }
 
